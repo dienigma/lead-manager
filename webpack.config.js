2c671c9c14d21656b6210ce707b7ec80c1ff3849
dienigma <joshi.chinmay09@gmail.com>
2019-12-18 07:43:03 +0530

    Finished up the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
-    }
-  ]
+    ]
+  }
 };
